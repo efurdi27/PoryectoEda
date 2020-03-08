ca165b09d51c5ddc863fa976d9af7d24ddb1e023
Eduardo Romero <eduardoromero@MacBook-Air-de-Eduardo.local>
2020-03-07 22:22:36 -0600

    Clase principal

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File text = new File("/Users/eduardoromero/Documents/PracticasPoo/Practica3/src/datos.txt");
+        Scanner sc = new Scanner(text);
+
+    }
+}
